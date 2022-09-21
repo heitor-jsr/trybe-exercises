614a21af79d4e7f62a9c303c30d6ee3b3ff233fa
heitor-jsr <heitorscalon@gmail.com>
2022-09-21 20:09:51 -0300

    exercicios

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio_06_maior_2_numeros.js
@@ -0,0 +1,10 @@
+// 2 - Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+
+const a = 10;
+const b = 100;
+
+if (a > b) {
+    console.log(`o valor de ${a} é maior que o valor de ${b}`)
+} else {
+    console.log(`o valor de ${b} é maior que o valor de ${a}`)
+}
---
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio_08_positivo_negativo.js
@@ -0,0 +1,11 @@
+// 4 - Utilize if/else para fazer um programa que, dado um valor recebido como parâmetro, retorne “positive” se esse valor for positivo, “negative” se for negativo, e caso não seja nem positivo e nem negativo retorne “zero”. 
+
+let a = -1;
+
+if (a > 0) {
+    console.log("positive")
+} else if (a < 0) {
+    console.log("negativo")
+} else {
+    console.log("zero") 
+}
