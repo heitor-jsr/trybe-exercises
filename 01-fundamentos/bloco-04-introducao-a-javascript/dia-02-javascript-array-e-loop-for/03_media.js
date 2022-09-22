734a01a87b39a124bc4f0cc77ebffbd5966f2e49
heitor-jsr <heitorscalon@gmail.com>
2022-09-22 17:16:14 -0300

    3 primeiros exercicios

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/03_media.js
@@ -0,0 +1,12 @@
+// Calcule e imprima a média aritmética dos valores contidos no array; 
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+let media = 0;
+
+for (index = 0; index < numbers.length; index += 1) {
+    sum += numbers[index];
+    media = sum / numbers.length;
+}
+
+console.log(`média = ${sum} / ${numbers.length}, que é ${media}.`)
