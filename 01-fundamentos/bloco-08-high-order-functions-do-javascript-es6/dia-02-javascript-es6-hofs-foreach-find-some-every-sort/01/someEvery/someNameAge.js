86b76b234e34dd6f79b73bdf2c470895e5959ccc
heitor-jsr <heitorscalon@gmail.com>
2022-10-25 11:19:57 -0300

    para fixar

---
+++ 01-fundamentos/bloco-08-high-order-functions-do-javascript-es6/dia-02-javascript-es6-hofs-foreach-find-some-every-sort/01/someEvery/someNameAge.js
@@ -0,0 +1,14 @@
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => {
+  if (arr.every((element) => element.age >= minimumAge)) return true;
+  return false
+};
+
+console.log(verifyAges(people, 18));
